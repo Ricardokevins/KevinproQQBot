264deade76b954322b4047a0f5604917ec4c33b8
kevinpro <43642508+Ricardokevins@users.noreply.github.com>
2021-08-20 21:48:14 +0800

    update

--- Kevinpro/bot_plugins/NJUQA.py
+++ Kevinpro/bot_plugins/NJUQA.py
@@ -34,8 +34,44 @@ async def _(session: CommandSession):
         result = await func(Ask)
     except ServiceException as e:
         result = e.message
-    
-    
-    
     await session.send(result)
 
+from aiocqhttp import MessageSegment
+# @on_command('setu', aliases=('富婆','色图', '老婆', '老婆图', '萝莉'))
+# async def setu(session: CommandSession):
+#     seq = MessageSegment.image("file:///D:\\KevinproPython\\workspace\\KevinproQQBot\\Kevinpro\\8531.png")
+#     print(seq)
+#     await session.send(seq)
+
+Base_path = "file:///D:\\KevinproPython\\workspace\\KevinproQQBot\\Kevinpro\\{}"
+from nonebot import on_command, CommandSession
+from nonebot import on_natural_language, NLPSession, IntentCommand
+from jieba import posseg
+import requests
+import time
+import urllib
+from lxml import etree
+from aiocqhttp import MessageSegment
+import requests
+
+
+class GetPic:
+    def __init__(self):
+        self.session = requests.session()
+
+    def get_Pic(self):
+        res = self.session.get("http://api.mtyqx.cn/api/random.php", verify=False)
+
+        # 保存图片
+        with open("{}.png".format('8531'), "wb") as f:
+            f.write(res.content)
+        return True
+
+@on_command('setu', aliases=('富婆','色图', '老婆', '老婆图', '萝莉'))
+async def setu(session: CommandSession):
+    Pic = GetPic()
+    import os
+    print(os.path.exists("{}.png".format('8531')))
+    if Pic.get_Pic():
+        seq = MessageSegment.image(Base_path.format('8531.png'))
+        await session.send(seq)
---
+++ Spider/wordcloud.py
@@ -0,0 +1,20 @@
+import jieba
+import wordcloud
+
+# 构建并配置词云对象w
+w = wordcloud.WordCloud(width=1000,
+                        height=700,
+                        background_color='white',
+                        font_path='msyh.ttc')
+
+# 对来自外部文件的文本进行中文分词，得到string
+f = open('关于实施乡村振兴战略的意见.txt',encoding='utf-8')
+txt = f.read()
+txtlist = jieba.lcut(txt)
+string = " ".join(txtlist)
+
+# 将string变量传入w的generate()方法，给词云输入文字
+w.generate(string)
+
+# 将词云图片导出到当前文件夹
+w.to_file('output5-village.png')
